bc69bf036ea91ed64399854291a4980db3adcec6
nanduugithub <nanduvs01@gmail.com>
2023-12-05 12:34:55 +0530

    adding changes

--- src/test/java/org/selenium/commands/Commands.java
+++ src/test/java/org/selenium/commands/Commands.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Commands extends Base {
 
     @Test
@@ -88,7 +90,66 @@ public class Commands extends Base {
         WebElement login= driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a"));
         login.click();
         WebElement email= driver.findElement(By.xpath("//*[@id=\"Email\"]"));
-        
-
+        email.sendKeys("nanduvs01@gmail.com");
+        WebElement password=driver.findElement(By.xpath("//*[@id=\"Password\"]"));
+        password.sendKeys("123456");
+        WebElement loginButton= driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input"));
+        loginButton.click();
+        WebElement actualUserLoginElement= driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[1]/a"));
+        String actualId= actualUserLoginElement.getText();
+        String expectedLoginId="nanduvs01@gmail.com";
+        Assert.assertEquals(actualId,expectedLoginId,"user Login failed");
     }
+    @Test
+    public void validateDemoWebShopSelector(){
+        driver.get("https://demowebshop.tricentis.com/");
+        WebElement login= driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.header > div.header-links-wrapper > div.header-links > ul > li:nth-child(2) > a"));
+        login.click();
+        WebElement email= driver.findElement(By.cssSelector("#Email"));
+        email.sendKeys("nanduvs01@gmail.com");
+        WebElement password=driver.findElement(By.cssSelector("#Password"));
+        password.sendKeys("123456");
+        WebElement loginButton= driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.master-wrapper-main > div.center-2 > div > div.page-body > div.customer-blocks > div.returning-wrapper > div.form-fields > form > div.buttons > input"));
+        loginButton.click();
+        WebElement actualUserLoginElement= driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.header > div.header-links-wrapper > div.header-links > ul > li:nth-child(1) > a"));
+        String actualId= actualUserLoginElement.getText();
+        String expectedLoginId="nanduvs01@gmail.com";
+        Assert.assertEquals(actualId,expectedLoginId,"user Login failed");
+    }
+    @Test
+    public void validateTotalNumOfTagInApplication(){
+        driver.get("https://demowebshop.tricentis.com/");
+        List<WebElement> inputWebElement=driver.findElements(By.tagName("Input"));
+        int size= inputWebElement.size();
+        System.out.println("total no of input tag in UI is "+size);
+    }
+@Test
+    public void validateDemoWebShopNavigate(){
+        driver.get("https://demowebshop.tricentis.com/");
+        WebElement register=driver.findElement(By.xpath("//a[@class='ico-register']"));
+        register.click();
+        driver.navigate().back();
+        driver.navigate().forward();
+        WebElement firstName= driver.findElement(By.xpath("//input[@name='FirstName']"));
+        firstName.sendKeys("nandu");
+        WebElement lastName= driver.findElement(By.xpath("//input[@name='LastName']"));
+        lastName.sendKeys("V S");
+        WebElement email= driver.findElement(By.xpath("//input[@name='Email']"));
+        email.sendKeys("nanduvs01@gmail.com");
+        driver.navigate().refresh();
+        driver.navigate().to("https://www.google.com/");
+}
+@Test
+public void verifyIsSelected() {
+    boolean isMaleSelected;
+    driver.get("https://demowebshop.tricentis.com/register");
+    WebElement male = driver.findElement(By.xpath("//input[@id='gender-male']"));
+    isMaleSelected = male.isSelected();
+    Assert.assertFalse(isMaleSelected,"checkbox is selected");
+    System.out.println("gender male before selected" + isMaleSelected);
+    male.click();
+    isMaleSelected = male.isSelected();
+    Assert.assertTrue(isMaleSelected,"checkbox is not selected");
+    System.out.println("gender after selected" + isMaleSelected);
+}
 }
